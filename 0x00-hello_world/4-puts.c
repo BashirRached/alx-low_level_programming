a3e188041f54d06c340eeb0cfaddeddfc394c7de
BashirRached <karche475b@gmail.com>
2022-12-08 12:58:00 -0800

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/*
+ * main - print a line of code using puts
+ *
+ * Return: 0
+*/
 int main(void)
 {
-	puts("/"Programming is like a multilingual puzzle");
+	puts("\"Programming is like a multilingual puzzle");
 	return (0);
 }
