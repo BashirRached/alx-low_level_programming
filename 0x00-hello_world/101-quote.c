04ae0680cf0ce6dc7fcbaab01d50f60a65b40f49
BashirRached <karche475b@gmail.com>
2022-12-11 22:44:46 -0800

    ME

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
+ * main - prints exactly and that piece of art is useful.
  *
- * Reurn: Alway 0 (success)
+ * Reurn: Alway 1.
 */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
